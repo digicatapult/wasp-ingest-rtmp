fb7d69c3fd51ec56667bc5b70a5fe3fce45c7fc7
Daniel Cooper <danielcooperxyz@gmail.com>
2022-03-09 13:34:48 +0000

    Add payload queue

--- services/kafka.go
+++ services/kafka.go
@@ -4,13 +4,21 @@ import (
 	"github.com/Shopify/sarama"
 )
 
+// Payload defines the data contained in
+type Payload struct {
+}
+
 type KafkaOperations interface {
 	SendMessage()
+
+	PayloadQueue() chan<- *Payload
 }
 
 // KafkaService is a ..
 type KafkaService struct {
 	ap sarama.AsyncProducer
+
+	payloads chan *Payload
 }
 
 type KafkaMessage struct {
@@ -26,4 +34,9 @@ func NewKafkaService(ap sarama.AsyncProducer) *KafkaService {
 
 func (k *KafkaService) SendMessage() {
 
+}
+
+// PayloadQueue provides access to load a payload object into the queue for sending
+func (k *KafkaService) PayloadQueue() chan<- *Payload {
+	return k.payloads
 }

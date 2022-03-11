aa4ee9c7458184933140ed4ccf8507c86396601a
Daniel Cooper <danielcooperxyz@gmail.com>
2022-03-11 16:16:05 +0000

    Fix lint statements

--- services/video-ingest.go
+++ services/video-ingest.go
@@ -30,43 +30,7 @@ func (vs *VideoIngestService) IngestVideo(rtmpURL string) {
 
 	go vs.ks.StartBackgroundSend(videoSendWaitGroup, shutdown)
 
-	go func() {
-		frameSize := 1000
-		frameCount := 0
-		buf := make([]byte, frameSize)
-
-		for {
-			count, err := io.ReadFull(pipeReader, buf)
-			frameCount++
-
-			switch {
-			case count == 0 || errors.Is(err, io.EOF):
-				zap.S().Debug("end of stream reached")
-
-				return
-			case count != frameSize:
-				zap.S().Infof("end of stream reached, sending short chunk: %d, %s", count, err)
-			case err != nil:
-				zap.S().Errorf("read error: %d, %s", count, err)
-				if count == 0 {
-					return
-				}
-			}
-
-			bufCopy := make([]byte, frameSize)
-			copy(bufCopy, buf)
-
-			payload := &Payload{
-				ID:      "the-stream-identifier",
-				FrameNo: frameCount * frameSize,
-				Data:    bufCopy,
-			}
-
-			zap.S().Debugf("Video chunk: %d - %d", payload.FrameNo, len(payload.Data))
-			videoSendWaitGroup.Add(1)
-			vs.ks.PayloadQueue() <- payload
-		}
-	}()
+	go vs.consumeVideo(pipeReader, videoSendWaitGroup)
 
 	done := make(chan error)
 
@@ -85,4 +49,43 @@ func (vs *VideoIngestService) IngestVideo(rtmpURL string) {
 	zap.S().Infof("Done (waiting for completion of send): %s", err)
 	videoSendWaitGroup.Wait()
 	shutdown <- true
+}
+
+func (vs *VideoIngestService) consumeVideo(reader io.Reader, videoSendWaitGroup *sync.WaitGroup) {
+	frameSize := 1000
+	frameCount := 0
+	buf := make([]byte, frameSize)
+
+	for {
+		count, err := io.ReadFull(reader, buf)
+		frameCount++
+
+		switch {
+		case count == 0 || errors.Is(err, io.EOF):
+			zap.S().Debug("end of stream reached")
+
+			return
+		case count != frameSize:
+			zap.S().Infof("end of stream reached, sending short chunk: %d, %s", count, err)
+		case err != nil:
+			zap.S().Errorf("read error: %d, %s", count, err)
+
+			if count == 0 {
+				return
+			}
+		}
+
+		bufCopy := make([]byte, frameSize)
+		copy(bufCopy, buf)
+
+		payload := &Payload{
+			ID:      "the-stream-identifier",
+			FrameNo: frameCount * frameSize,
+			Data:    bufCopy,
+		}
+
+		zap.S().Debugf("Video chunk: %d - %d", payload.FrameNo, len(payload.Data))
+		videoSendWaitGroup.Add(1)
+		vs.ks.PayloadQueue() <- payload
+	}
 }

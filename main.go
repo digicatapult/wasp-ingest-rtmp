daca581a8b26a912e55b5b4d99bacc7145207b43
Daniel Cooper <5233036+danielcooperxyz@users.noreply.github.com>
2022-03-11 14:37:36 +0000

    Add kafka payload queue (#5)
    * Add payload queue
    
    * Update linting rules
    
    * Fixed linting for everything
    
    * Push payload objects into queue
    
    * video ingestion messaging
    
    * Use sync producer to send messages
    
    * Refactor to tidy up background video sending
    
    * Fix linting

--- main.go
+++ main.go
@@ -3,23 +3,33 @@ package main
 import (
 	"log"
 	"os"
-	"os/signal"
 	"strings"
 
 	"github.com/Shopify/sarama"
+	"github.com/pkg/errors"
 
 	"github.com/digicatapult/wasp-ingest-rtmp/services"
 	"github.com/digicatapult/wasp-ingest-rtmp/util"
 )
 
-func setupProducer(kafkaBrokers []string) (sarama.AsyncProducer, error) {
-	return sarama.NewAsyncProducer(kafkaBrokers, nil)
+func setupProducer(kafkaBrokers []string) (sarama.SyncProducer, error) {
+	var (
+		producer sarama.SyncProducer
+		err      error
+	)
+
+	if producer, err = sarama.NewSyncProducer(kafkaBrokers, nil); err != nil {
+		return producer, errors.Wrap(err, "problem initiating producer")
+	}
+
+	return producer, nil
 }
 
 func main() {
 	sarama.Logger = log.New(os.Stdout, "[Sarama] ", log.LstdFlags)
 
 	kafkaBrokers := util.GetEnv(util.KafkaBrokersEnv, "localhost:9092")
+
 	producer, errProducer := setupProducer(strings.Split(kafkaBrokers, ","))
 	if errProducer != nil {
 		panic(errProducer)
@@ -31,23 +41,8 @@ func main() {
 		}
 	}()
 
-	// Trap SIGINT to trigger a graceful shutdown.
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, os.Interrupt)
-
 	kafka := services.NewKafkaService(producer)
 
-	messageKey := "01000000-0000-4000-8883-c7df300514ed"
-	messageValue := services.KafkaMessage{
-		Ingest:    "rtmp",
-		IngestID:  "4883C7DF300514ED",
-		Timestamp: "2021-08-31T14:51:36.507Z",
-		Payload:   "",
-		Metadata:  "{}",
-	}
-
-	kafka.SendMessage(messageKey, messageValue, signals)
-
-	// videoIngest := services.NewVideoIngestService()
-	// videoIngest.IngestVideo()
+	videoIngest := services.NewVideoIngestService(kafka)
+	videoIngest.IngestVideo()
 }

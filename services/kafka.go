e7a752b71b74959113ab6b7ff036f6d39c1d0b71
Daniel Cooper <danielcooperxyz@gmail.com>
2022-03-16 13:40:44 +0000

    Use payload id for ingest

--- services/kafka.go
+++ services/kafka.go
@@ -90,7 +90,7 @@ func (k *KafkaService) StartBackgroundSend(sendWaitGroup *sync.WaitGroup, shutdo
 			messageKey := "01000000-0000-4000-8883-c7df300514ed"
 			messageValue := KafkaMessage{
 				Ingest:    "rtmp",
-				IngestID:  "4883C7DF300514ED",
+				IngestID:  payload.ID,
 				Timestamp: "2021-08-31T14:51:36.507Z",
 				Payload:   base64.StdEncoding.EncodeToString(payload.Data),
 				Metadata:  "{}",
